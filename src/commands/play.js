7a7c161c2a9b9fcdff3b01bb5c64e66b5e203819
João Marcelo <joaomarcelolimaoliveira7@gmail.com>
2023-09-05 09:13:49 -0300

    play music and collect errors completed

--- src/commands/play.js
+++ src/commands/play.js
@@ -1,46 +1,71 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource, NoSubscriberBehavior } = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('play')
-        .setDescription('Comando de play de musicas'),
+        .setDescription('Comando de play de músicas')
+        .addStringOption((option) => option.setName('url').setDescription('URL da música').setRequired(true)),
     
-        async execute(interaction) {
-            const channel = interaction.member.voice.channelId;
-            
-            if (!channel) {
-                return interaction.reply('Você precisa estar em um canal de voz para usar este comando.');
-            }
+    async execute(interaction) {
+        const channel = interaction.member.voice.channelId;
         
-            const botVoiceState = interaction.member.voice;
-        
-            if (!botVoiceState) {
-                return interaction.reply('O bot não está em um canal de voz.');
-            }
-        
-            botVoiceState.selfMute = true;
-        
-            const connection = joinVoiceChannel({
-                channelId: channel,
-                guildId: interaction.guild.id,
-                adapterCreator: interaction.guild.voiceAdapterCreator,
-            });
-        
-            const player = createAudioPlayer({
-                behaviors: {
-                    noSubscriber: NoSubscriberBehavior.Play
-                }
-            });
-        
-            player.setMaxListeners(10);
-        
-            connection.subscribe(player);
-        
-            const audioResource = createAudioResource(require('path').join(__dirname, './lost-in-you.opus'));
-        
-            player.play(audioResource);
-        
-            interaction.reply('Reproduzindo áudio...');
+        if (!channel) {
+            return interaction.reply('Você precisa estar em um canal de voz para usar este comando.');
         }
-}
+
+        const botVoiceState = interaction.member.voice;
+
+        if (!botVoiceState) {
+            return interaction.reply('O bot não está em um canal de voz.');
+        }
+
+        const youtubeUrl = interaction.options.getString('url'); // Obtém a URL da opção do usuário
+
+        if (!youtubeUrl) {
+            return interaction.reply('Por favor, forneça uma URL válida da música.');
+        }
+
+        const connection = joinVoiceChannel({
+            channelId: channel,
+            guildId: interaction.guild.id,
+            adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+
+        const player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Play
+            }
+        });
+
+        player.setMaxListeners(10);
+
+        connection.subscribe(player);
+
+        // Use a função ytdl para fazer o download do áudio do vídeo do YouTube
+        const stream = ytdl(youtubeUrl, { filter: 'audioonly' });
+
+        console.log(stream);
+
+        const audioResource = createAudioResource(stream);
+
+        player.play(audioResource);
+
+        interaction.reply(
+            `
+            Tocando agora: ${youtubeUrl}
+            musica iniciada por: @${interaction.user.username}
+            
+            `
+        );
+
+        player.on('error', (error) => {
+            console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+            interaction.reply(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+        });
+
+    }
+};
+
+
