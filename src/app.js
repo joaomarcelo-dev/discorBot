1d6ce9aec82701e3fae1a940a974ad8365c92dfd
João Marcelo <joaomarcelolimaoliveira7@gmail.com>
2023-09-05 20:38:11 -0300

    fix: error vercel

--- src/app.js
+++ src/app.js
@@ -3,75 +3,63 @@ const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const {TOKEN, CLIENT_ID, GUILD_ID} = process.env;
+const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
-const client = new Client({ 
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildVoiceStates,
-    ] 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
 });
 
 // ==================== Import commands ==================== //
 
-const fs = require('node:fs');
-const path = require('node:path');
+const fs = require('fs');
+const path = require('path');
 
 const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'))
-
-// console.log(commandFiles);
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
 
 client.commands = new Collection();
 
-for (file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-    const command = require(filePath)
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
 
-    if ("data" in command && "execute" in command) {
-        client.commands.set(command.data.name, command)
-    } else {
-        console.log(`Esse commando em: ${filePath}, falhou`)
-    }
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`Esse comando em: ${filePath}, falhou`);
+  }
 }
-
-// console.log(client.commands);               
 
 // ---------------------------------------------------------- //
 
-client.once(Events.ClientReady, c => {
-	console.log(`Login realizado como: ${c.user.tag}`);
+client.once(Events.ClientReady, () => {
+  console.log(`Login realizado como: ${client.user.tag}`);
 });
-
-client.login(TOKEN);
-
 
 // ====================== Listner interactions ================= //
 
 client.on(Events.InteractionCreate, async (interaction) => {
-    if (!interaction.isChatInputCommand()) return;
-    
-    const command = interaction.client.commands.get(interaction.commandName);
+  if (!interaction.isChatInputCommand()) return;
 
+  const command = client.commands.get(interaction.commandName);
 
-    if(!command) {
-        console.error('Comando não encontrado!!!')
-        return;
-    }
+  if (!command) {
+    console.error('Comando não encontrado!!!');
+    return;
+  }
 
-    try {
-        await command.execute(interaction, client);
-    } catch (error) {
-        console.error(error)
-
-        interaction.reply('Houve um erro ao executar esse comando');
-    } finally {
-        console.log(`Comando ${command.data.name} executado`);
-    }
+  try {
+    await command.execute(interaction, client);
+  } catch (error) {
+    console.error(error);
+    interaction.reply('Houve um erro ao executar esse comando');
+  } finally {
+    console.log(`Comando ${command.data.name} executado`);
+  }
 });
 
-function startBot() {
-    client.login('YOUR_BOT_TOKEN');
-  }
-  
-  module.exports = startBot;
+// Start the bot
+client.login(TOKEN);
